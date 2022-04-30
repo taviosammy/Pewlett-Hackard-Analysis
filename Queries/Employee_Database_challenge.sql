a2519c22b050a0040586a91ef0f3cfb7ee390267
taviosammy <taviosammy@gmail.com>
2022-04-30 17:11:40 -0400

    update challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -16,3 +16,22 @@ INTO title_count
 FROM uni_titles AS ut
 Group BY ut.title
 ORDER BY COUNT(ut.title) DESC
+
+
+--Mentorship Program eligibility
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+        e.first_name,
+        e.last_name,
+        e.birth_date,
+        de.from_date,
+		de.to_date,
+        ti.title
+INTO mentor_eligibility
+FROM employees AS e
+LEFT JOIN  dept_emp AS de
+    ON e.emp_no = de.emp_no
+LEFT JOIN titles AS ti
+    ON e.emp_no = ti.emp_no
+WHERE (de.to_date = '9999-01-01') AND 
+(e.birth_date BETWEEN '1965-01-01' AND '1965-01-31')
+ORDER BY e.emp_no
